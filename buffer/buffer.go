dfaad2f1ef6f8d12b3ce65a23c2ac137f66b3b99
Isabelle Miller <izzy@stardust.fm>
2015-01-12 17:12:57 -0500

    Start preparing for initial take

--- buffer/buffer.go
+++ buffer/buffer.go
@@ -5,14 +5,40 @@
 // package buffer implements buffers representing views into a piece of text.
 package buffer
 
-import "io"
+import (
+	"io"
+	"io/ioutil"
+)
 
 type View interface {
 	ByteAtOffset(n int)
 	SetReadOffset(n int)
 	io.Reader
+	io.ReaderAt
 }
 
 type Buffer struct {
 	Contents View
+}
+
+type Line struct {
+	prev     *Line
+	next     *Line
+	contents []byte
+}
+
+type SmallFileBuffer struct {
+	Filename string
+	contents []byte
+}
+
+func BufferizeFile(filename string) (*SmallFileBuffer, error) {
+	a := new(SmallFileBuffer)
+	a.Filename = filename
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	a.contents = contents
+	return a, nil
 }
